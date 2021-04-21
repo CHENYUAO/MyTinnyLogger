8f14664272e3376f88797e010f9676e50bd60a99
Joe Chen <53866413+CHENYUAO@users.noreply.github.com>
2021-04-21 22:14:00 +0800

    实现异步写日志功能

--- mylogger/fileLogger.go
+++ mylogger/fileLogger.go
@@ -16,6 +16,16 @@ type FileLogger struct {
 	maxSize    int64
 }
 
+type MsgInfo struct {
+	level    string
+	msg      string
+	fileName string
+	now      string
+	line     int
+}
+
+var MsgChannel chan *MsgInfo = make(chan *MsgInfo, 50000)
+
 func NewFileLogger(levStr, fp, fn string, ms int64) *FileLogger {
 	ll, err := parse(levStr)
 	if err != nil {
@@ -29,13 +39,37 @@ func NewFileLogger(levStr, fp, fn string, ms int64) *FileLogger {
 	if err != nil {
 		panic("open err file failed")
 	}
-	return &FileLogger{
+	fl := &FileLogger{
 		Level:      ll,
 		filePath:   fp,
 		fileName:   fn,
 		fileObj:    fo,
 		errFileObj: errFo,
 		maxSize:    ms,
+	}
+	go fl.runGoroutine()
+	return fl
+}
+
+func (fl *FileLogger) runGoroutine() {
+	for {
+		m, ok := <-MsgChannel
+		if ok {
+			//debug
+			fmt.Printf("before:%v\n", fl.fileObj)
+			fl.fileObj = fl.checkSize(fl.fileObj)
+			fmt.Printf("after :%v\n", fl.fileObj)
+			fmt.Fprintf(fl.fileObj, "[%s] [%s] [%s:%d]: %s\n", m.now, m.level, m.fileName, m.line, m.msg)
+			l, err := parse(m.level)
+			if err != nil {
+				fmt.Println("unparse level failed")
+				continue
+			}
+			if l >= ERROR {
+				fl.errFileObj = fl.checkSize(fl.errFileObj)
+				fmt.Fprintf(fl.errFileObj, "[%s] [%s] [%s:%d]: %s\n", m.now, m.level, m.fileName, m.line, m.msg)
+			}
+		}
 	}
 }
 
@@ -47,14 +81,21 @@ func (fl *FileLogger) fileLog(lv LogLevel, msg string, a ...interface{}) {
 		panic(err)
 	}
 	now := time.Now()
-	fl.fileObj = fl.checkSize(fl.fileObj)
-	fl.fileObj = fl.checkDate(fl.fileObj)
-	fmt.Printf("fileObj:%v\n", fl.fileObj)
-	fmt.Fprintf(fl.fileObj, "[%s] [%s] [%s:%d]: %s\n", now.Format("2006-01-02 15:04:05"), levelStr, fileName, line, msg)
-	fl.errFileObj = fl.checkSize(fl.errFileObj)
-	fl.errFileObj = fl.checkDate(fl.errFileObj)
-	if lv >= ERROR {
-		fmt.Fprintf(fl.errFileObj, "[%s] [%s] [%s:%d]: %s\n", now.Format("2006-01-02 15:04:05"), levelStr, fileName, line, msg)
+	// fl.fileObj = fl.checkSize(fl.fileObj)
+	// fl.fileObj = fl.checkDate(fl.fileObj)
+	// fmt.Printf("fileObj:%v\n", fl.fileObj)
+	// fmt.Fprintf(fl.fileObj, "[%s] [%s] [%s:%d]: %s\n", now.Format("2006-01-02 15:04:05"), levelStr, fileName, line, msg)
+	// fl.errFileObj = fl.checkSize(fl.errFileObj)
+	// fl.errFileObj = fl.checkDate(fl.errFileObj)
+	// if lv >= ERROR {
+	// 	fmt.Fprintf(fl.errFileObj, "[%s] [%s] [%s:%d]: %s\n", now.Format("2006-01-02 15:04:05"), levelStr, fileName, line, msg)
+	// }
+	MsgChannel <- &MsgInfo{
+		level:    levelStr,
+		msg:      msg,
+		fileName: fileName,
+		now:      now.Format("2006-01-02 15:04:05"),
+		line:     line,
 	}
 }
 
@@ -78,29 +119,29 @@ func (fl *FileLogger) checkSize(file *os.File) *os.File {
 	return file
 }
 
-func (fl *FileLogger) checkDate(file *os.File) *os.File {
-	fileInfo, err := file.Stat()
-	if err != nil {
-		panic("get file info failed")
-	}
-	fileTime := fileInfo.ModTime()
-	fileYear, fileMonth, fileDay := fileTime.Date()
-	now := time.Now()
-	year, month, day := now.Date()
-	if year != fileYear || month != fileMonth || day != fileDay {
-		nowStr := time.Now().Format("2006_0102_1504_05000")
-		oldName := path.Join(fl.filePath, fileInfo.Name())
-		newName := fmt.Sprintf("%s.bak.%s", oldName, nowStr)
-		file.Close()
-		os.Rename(oldName, newName)
-		newFile, err := os.OpenFile(oldName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			panic("open new file failed")
-		}
-		return newFile
-	}
-	return file
-}
+// func (fl *FileLogger) checkDate(file *os.File) *os.File {
+// 	fileInfo, err := file.Stat()
+// 	if err != nil {
+// 		panic("get file info failed")
+// 	}
+// 	fileTime := fileInfo.ModTime()
+// 	fileYear, fileMonth, fileDay := fileTime.Date()
+// 	now := time.Now()
+// 	year, month, day := now.Date()
+// 	if year != fileYear || month != fileMonth || day != fileDay {
+// 		nowStr := time.Now().Format("2006_0102_1504_05000")
+// 		oldName := path.Join(fl.filePath, fileInfo.Name())
+// 		newName := fmt.Sprintf("%s.bak.%s", oldName, nowStr)
+// 		file.Close()
+// 		os.Rename(oldName, newName)
+// 		newFile, err := os.OpenFile(oldName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+// 		if err != nil {
+// 			panic("open new file failed")
+// 		}
+// 		return newFile
+// 	}
+// 	return file
+// }
 
 func (fl *FileLogger) Debug(msg string, a ...interface{}) {
 	if fl.Level <= DEBUG {
--- test/main.go
+++ test/main.go
@@ -5,17 +5,18 @@ import (
 	"time"
 )
 
-var log mylogger.Logger
+//var log mylogger.Logger
 
 func main() {
-	//log = mylogger.NewFileLogger("DEBUG", "./", "test.log", 10*1024*1024)
-	log = mylogger.NewConsoleLogger("DEBUG")
-	log.Debug("this is a DEBUG message")
-	log.Trace("this is a TARCE message")
-	log.Warning("this is a WARN message")
-	log.Error("this is a ERROR message")
-	log.Fatal("this is a FATAL message")
-	time.Sleep(time.Millisecond * 100)
-	//log.Close()
-
+	log := mylogger.NewFileLogger("DEBUG", "./", "test.log", 10*1024)
+	for {
+		//log = mylogger.NewConsoleLogger("DEBUG")
+		log.Debug("this is a DEBUG message")
+		log.Trace("this is a TARCE message")
+		log.Warning("this is a WARN message")
+		log.Error("this is a ERROR message")
+		log.Fatal("this is a FATAL message")
+		time.Sleep(time.Millisecond * 100)
+		//log.Close()
+	}
 }
